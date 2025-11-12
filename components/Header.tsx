fc56f75b1ed20dbb70b6053cbf95a50a4bb795bb
Kuba <kubasatora@wp.pl>
2025-11-12 15:20:49 +0100

    href optimalization

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,21 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Navitems from './Navitems'
 
 const header = () => {
   return (
     <header className='sticky top-0 header'>
-      <div className='container header-wrapper'> 
-        <Link href ='/'>
-        <Image src="/assets/icons/logo.svg" alt='signalist logo' width={140} height={32} className='h-8 w-auto cursor-pointer'/>
+      <div className='container header-wrapper'>
+        <Link href='/'>
+          <Image src="/assets/icons/logo.svg" alt='signalist logo' width={140} height={32} className='h-8 w-auto cursor-pointer' />
         </Link>
         <nav className='hidden sm:block'>
-{/* Future navigation items can be added here */}
+          <Navitems />
         </nav>
         {/* Future user actions can be added here */}
-        </div>
-        </header>
+      </div>
+    </header>
   )
 }
 
