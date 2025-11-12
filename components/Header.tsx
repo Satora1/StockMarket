a4f66a4a99e63eed746025ee5bc7cb9e86abdda8
Kuba <kubasatora@wp.pl>
2025-11-12 12:51:57 +0100

    header assets

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const header = () => {
   return (
-    <div>header</div>
+    <header className='sticky top-0 header'>
+      <div className='container header-wrapper'> 
+        <Link href ='/'>
+        <Image src="/assets/icons/logo.svg" alt='signalist logo' width={140} height={32} className='h-8 w-auto cursor-pointer'/>
+        </Link>
+        <nav className='hidden sm:block'>
+{/* Future navigation items can be added here */}
+        </nav>
+        {/* Future user actions can be added here */}
+        </div>
+        </header>
   )
 }
 
